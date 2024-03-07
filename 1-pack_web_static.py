56b2ab27eca3a9d358e44f8d74a9e59ac7f4838b
khaledolemy <khaledolemy@gmail.com>
2024-03-07 13:05:47 +0200

    1fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 # Compress a folder and return path to .tgz
 import datetime
-import os.path
+import os
 from fabric.api import local
 
 
 def do_pack():
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    filename = str(datetime.datetime.now()).split('.')[0].replace('-', '')
+    filename = filename.replace(' ', '').replace(':', '')
+    filename = f"versions/web_static_{filename}.tgz"
     if not os.path.isdir("versions"):
         if local("mkdir versions").failed:
             return None
     status = local(f"tar -cvzf {filename} web_static")
     if status.failed:
         return None
-    else:
-        return filename
+    return filename
