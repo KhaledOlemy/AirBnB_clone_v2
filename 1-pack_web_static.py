af9f4ac02d77cbcc7a84344bc875f5b3280d12a7
khaledolemy <khaledolemy@gmail.com>
2024-03-07 13:04:02 +0200

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,13 @@ from fabric.api import local
 
 
 def do_pack():
-    filename = str(datetime.datetime.now()).split('.')[0].replace('-', '')
-    filename = filename.replace(' ', '').replace(':', '')
-    filename = f"versions/web_static_{filename}.tgz"
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
     if not os.path.isdir("versions"):
         if local("mkdir versions").failed:
             return None
