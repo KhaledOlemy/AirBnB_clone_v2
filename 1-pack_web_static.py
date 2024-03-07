cce6718393fe5ca66cb0328b195f02ea70f64103
khaledolemy <khaledolemy@gmail.com>
2024-03-07 19:44:01 +0200

    addd

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,12 +12,11 @@ def do_pack():
     Pack dir to .tgz
     """
     try:
-    	filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    	filename = f"versions/web_static_{filename}.tgz"
+        filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"versions/web_static_{filename}.tgz"
         if not os.path.isdir("versions"):
             local("mkdir versions")
         local(f"tar -cvzf {filename} web_static")
         return filename
     except:
         return None
-
