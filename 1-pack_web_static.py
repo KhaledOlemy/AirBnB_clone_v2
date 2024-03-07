aee22cd786c28043a51915e92d031feb73fca5a5
khaledolemy <khaledolemy@gmail.com>
2024-03-07 10:59:02 +0200

    local_tar

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Compress a folder and return path to .tgz
+import datetime
+import os
+import fabric.api
+def do_pack():
+    filename = str(datetime.datetime.now()).split('.')[0].replace('-', '').replace(' ', '').replace(':', '')
+    filename = f"web_static_{filename}.tar.gz"
+    if not os.path.isdir("versions"):
+        status = fabric.api.local("mkdir versions").succeeded
+        if not status:
+            return None
+    status = fabric.api.local(f"tar -czvf versions/{filename}.tar.gz webstatic/").succeeded
+    if not status:
+        return None
+    else:
+        return f"versions/{filename}.tar.gz"
