9a4287f0380f203c00d005ddddfc23b8f55014c1
khaledolemy <khaledolemy@gmail.com>
2024-03-07 12:09:36 +0200

    fix_tar_err_filename

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,20 +2,24 @@
 # Compress a folder and return path to .tgz
 import datetime
 import os
-from fabric.api import local
+import fabric
 
 
 def do_pack():
     filename = str(datetime.datetime.now()).split('.')[0].replace('-', '')
     filename = filename.replace(' ', '').replace(':', '')
-    filename = f"web_static_{filename}.tar.gz"
+    filename = f"web_static_{filename}.tgz"
+    print(filename)
+    local = fabric.api.local
     if not os.path.isdir("versions"):
-        status = fabric.api.local("mkdir versions").succeeded
+        status = local("mkdir versions").succeeded
+        print(status)
         if not status:
             return None
-    status = local(f"tar -czvf versions/{filename}.tar.gz webstatic/")
+    status = local(f"tar -czvf versions/{filename} web_static")
     status = status.succeeded
+    print(status)
     if not status:
         return None
     else:
-        return f"versions/{filename}.tar.gz"
+        return f"versions/{filename}.tgz"
