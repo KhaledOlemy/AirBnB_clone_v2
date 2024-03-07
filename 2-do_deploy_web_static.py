bab0e72c58375f09f76cc85797fff8b0ef819ca3
khaledolemy <khaledolemy@gmail.com>
2024-03-07 18:46:01 +0200

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # configure the website to the new release of the website
 
-from fabric.api import put, run, env
-from os.path import exists
+from fabric.api import run, put, env
 import os
 env.hosts = ['3.83.245.203', '54.173.35.201']
 
@@ -12,17 +11,17 @@ def do_deploy(archive_path):
     if os.path.isfile(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        c_path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/{}'.format(filename))
+        run('mkdir -p {}{}/'.format(c_path, dir_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, c_path, dir_name))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}{}/web_static/* {}{}/'.format(c_path, dir_name, c_path, dir_name))
+        run('rm -rf {}{}/web_static'.format(c_path, dir_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(c_path, dir_name))
         return True
     except:
         return False
