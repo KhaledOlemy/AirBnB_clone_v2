c52d7463fb8c163d8827df66c39df4fcabf7b908
khaledolemy <khaledolemy@gmail.com>
2024-03-07 17:46:48 +0200

    try_except

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
-from fabric.api import put, run, env
-from os.path import exists
+# configure the website to the new release of the website """
+import os
+from fabric.api import run, put
+from fabric.api import env
 env.hosts = ['3.83.245.203', '54.173.35.201']
 
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ Deploy the content of the tar onto your servers"""
+    if not os.path.isfile(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+    filename = archive_path.split('/')[-1]
+    dir_name = filename.split('.')[0]
+    if put(archive_path, f'/tmp/{filename}').failed:
         return False
+    if run(f"mkdir -p /data/web_static/releases/{dir_name}/").failed:
+        return False
+    d1 = f"/data/web_static/releases/{dir_name}/"
+    if run(f"tar -xzf /tmp/{filename} -C {d1}").failed:
+        return False
+    if run(f"rm /tmp/{filename}").failed:
+        return False
+    d1 = f"/data/web_static/releases/{dir_name}/web_static/*"
+    d2 = f"/data/web_static/releases/{dir_name}/"
+    if run(f"mv {d1} {d2}").failed:
+        return False
+    if run(f"rm -rf /data/web_static/releases/{dir_name}/web_static").failed:
+        return False
+    if run(f"rm -rf /data/web_static/current").failed:
+        return False
+    d1 = f"/data/web_static/releases/{dir_name}/"
+    d2 = f"/data/web_static/current"
+    if run(f"ln -s {d1} {d2}").failed:
+        return False
+    print("New version deployed!")
+    return True
