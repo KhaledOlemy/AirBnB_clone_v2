01ba14a43adae64c5573362223051e1929e2bc16
khaledolemy <khaledolemy@gmail.com>
2024-03-07 19:29:30 +0200

    whole_done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
-
-from datetime import datetime
+""" Compress a folder and return path to .tgz """
+import datetime
+import os
 from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+    """
+    Pack dir to .tgz
+    """
+    filename = str(datetime.datetime.now()).split('.')[0].replace('-', '')
+    filename = filename.replace(' ', '').replace(':', '')
+    filename = f"versions/web_static_{filename}.tgz"
+    if not os.path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    status = local(f"tar -cvzf {filename} web_static")
+    if status.failed:
         return None
+    else:
+        return filename
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,42 +11,51 @@ env.hosts = ['3.83.245.203', '54.173.35.201']
 
 
 def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+    """
+    Pack dir to .tgz
+    """
+    filename = str(datetime.datetime.now()).split('.')[0].replace('-', '')
+    filename = filename.replace(' ', '').replace(':', '')
+    filename = f"versions/web_static_{filename}.tgz"
+    if not os.path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    status = local(f"tar -cvzf {filename} web_static")
+    if status.failed:
         return None
+    else:
+        return filename
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """configure the website to the new release of the website"""
+    if isfile(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        file_name = archive_path.split("/")[-1]
+        dir_name = file_name.split(".")[0]
+        cmn_path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(cmn_path, dir_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, cmn_path, dir_name))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}{}/web_static/* {}{}/'.
+            format(cmn_path, dir_name, cmn_path, dir_name))
+        run('rm -rf {}{}/web_static'.format(cmn_path, dir_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(cmn_path, dir_name))
+        print("New version deployed!")
         return True
     except:
         return False
 
 
+f = do_pack()
+
+
 def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+    """2 calls at once, more automated"""
+    if not f:
         return False
-    return do_deploy(archive_path)
+    return do_deploy(f)
+
