d8caf694d77dc36a3b68a48b7033dac59954105e
khaledolemy <khaledolemy@gmail.com>
2024-03-23 00:52:19 +0200

    exec_code_on_code_not_cmd

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Basic task to handle the root of  a webserver
+"""
+Basic task to handle the root of  a webserver
+"""
 from flask import Flask
 
 my_app = Flask(__name__)
@@ -10,3 +12,6 @@ def hello_bnbn():
     return Hello HBNB! when requesting the root of the domain
     """
     return "Hello HBNB!"
+
+if __name__ == "__main__":
+    my_app.run(host="0.0.0.0", port="5000")
