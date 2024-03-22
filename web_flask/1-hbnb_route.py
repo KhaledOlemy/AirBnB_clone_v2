a15ceb52cc719277c7a9f950302127c5a3f888a4
khaledolemy <khaledolemy@gmail.com>
2024-03-23 00:55:57 +0200

    custom_path

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Basic task to handle the root of  a webserver
+"""
+from flask import Flask
+
+my_app = Flask(__name__)
+
+
+@my_app.route("/", strict_slashes=False)
+def hello_bnbn():
+    """
+    return Hello HBNB! when requesting the root of the domain
+    """
+    return "Hello HBNB!"
+
+
+@my_app.route("/hbnb", strict_slashes=False)
+def sub_path():
+    """
+    return HBNB when requesting subsite
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    my_app.run(host="0.0.0.0", port="5000")
