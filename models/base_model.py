12e40023da592fcf78dbb923fdb693993a016b87
khaledolemy <khaledolemy@gmail.com>
2024-03-23 23:59:45 +0200

    I_FIXED_IT

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import mapped_column
 import models
 
 Base = declarative_base()
@@ -11,10 +12,12 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-
+    # id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    # created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    # updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    id = mapped_column(String(60), unique=True, nullable=False, primary_key=True, sort_order=-3)
+    created_at = mapped_column(DateTime, nullable=False, default=(datetime.utcnow()), sort_order=-2)
+    updated_at = mapped_column(DateTime, nullable=False, default=(datetime.utcnow()), sort_order=-1)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,12 +4,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
-from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
+from models.user import User
+from models.place import Place
 from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
